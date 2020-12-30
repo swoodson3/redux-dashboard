cec490755522eb6f278c0913bca512325a6d8272
Kris Szafranski <kris@thoughtshelter.com>
2020-12-30 14:25:52 -0600

    converted dashboard comp

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // THIS COMPONENT IS OUR STATUS PAGE
 // YOU SHOULD DISPLAY THE CURRENT SPEED FROM SECTION ONE
 // YOU SHOULD DISPLAY THE COUNT OF PEOPLE CURRENTLY ON BOARD
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Dashboard</h2>
-        
-        <p>SPEED: GOES HERE</p>
-        <p>PASSENGER COUNT: GOES HERE</p>
-      </div>
-    )
-  }
+function Dashboard() {
+  
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <p>SPEED: GOES HERE</p>
+      <p>PASSENGER COUNT: GOES HERE</p>
+    </div>
+  );
 }
 
 export default Dashboard;
